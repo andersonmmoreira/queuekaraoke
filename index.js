f57beefe380a752d1e7abf8ea9bd25fad3f3be47
Marcio Ferreira <hmarciogferreira@gmail.com>
2024-08-09 19:20:27 -0300

    Ajustes Finais

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv'
-
-dotenv.config()
+import { v4 as uuidv4 } from 'uuid';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const lista = [
-    { id: 1, name: 'Song 1', music: 'Artist 1', table: '10' },
-    { id: 2, name: 'Song 2', music: 'Artist 2', table: '10' },
-    { id: 3, name: 'Song 3', music: 'Artist 3', table: '10' },
-    { id: 4, name: 'Song 4', music: 'Artist 4', table: '10' },
-
-];
+const lista = [];
 
 app.get('/', (req, res) => {
-    res.sendFile(process.env.INIT_CWD + '/index.html');
+    
+    return res.send("Hello Karaoke")
 })
 
 app.get('/musicas', (req, res) => {
@@ -26,6 +19,8 @@ app.get('/musicas', (req, res) => {
 
 app.post('/musicas', (req, res) => {
     const body = req.body;
+    const myUUID = uuidv4();
+    body.id = myUUID;
     lista.push(body);
     return res.status(200).json({ message: 'Música Salva com Sucesso.'})
 })
@@ -34,21 +29,18 @@ app.put('/musicas/:indice', (req, res) => {
     const indice = req.params.indice;
     const body = req.body;
     const indiceLista = lista.findIndex(indiceLista => indice === indiceLista)
-    lista[indiceLista] = body;
     return res.status(200).json({ message: 'Música Atualizada com Sucesso.'})
 })
 
 app.delete('/musicas/:id', (req, res) => {
-    const indice = req.params.indice;
+    const id = req.params.id;
+    const indice = lista.findIndex(item => item.id === id)
     lista.splice(indice, 1);
     return res.status(200).json({ message: 'Música Removida com Sucesso.'})
-})
-
-app.get('/', (req, res) => {
-    res.sendFile('C:/Users/Thaila/Documents/PROJETOS_SOFTWARE/karaoke/index.html');
 })
 
 app.listen(3000, 'localhost', () => {
     console.log('Server is running on port http://localhost:3000');
 });
 
+export default app;
